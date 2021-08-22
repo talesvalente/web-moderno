edcf10dd8b578645bd56b4c075ecec046de9fc8e
Tales Valente <talesvalente@gmail.com>
2021-08-22 15:47:36 -0300

    Create string.js
    Operações com STRINGS

---
+++ EXERCICIOS-JS/FUNDAMENTOS/string.js
@@ -0,0 +1,9 @@
+const escola = "KANTOBYTE3"
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('3'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0,3))
